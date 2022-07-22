5f7bf7bcea09615147e337f9e86fb8c45b915d47
zubadestroyer1 <88352982+zubadestroyer1@users.noreply.github.com>
2022-07-21 17:44:27 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="DeepLearningSeries",
+  author="Yiqiao Yin",
+  author_email="Eagle0504@gmail.com",
+  license="MIT license",
+  description="This package provides AI-driven solutions.",
+  packages=["DeepLearning"]
+)
